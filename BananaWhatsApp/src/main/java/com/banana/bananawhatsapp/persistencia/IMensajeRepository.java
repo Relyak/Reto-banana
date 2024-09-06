5bc484920dc96c7acbf3d53f14285cad04d6edc9
kayler maquinavirtual <kaylerdavid@gmail.com>
2024-09-06 15:34:56 +0200

    cambios en rama kayler

--- BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/persistencia/IMensajeRepository.java
+++ BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/persistencia/IMensajeRepository.java
@@ -2,16 +2,23 @@ package com.banana.bananawhatsapp.persistencia;
 
 import com.banana.bananawhatsapp.modelos.Mensaje;
 import com.banana.bananawhatsapp.modelos.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.sql.SQLException;
 import java.util.List;
 
-public interface IMensajeRepository {
-    public Mensaje crear(Mensaje mensaje) throws SQLException;
+public interface IMensajeRepository extends JpaRepository<Mensaje,Integer> {
+    //public Mensaje addMensaje(Mensaje mensaje) throws SQLException;
+    //modificado este metodo porque no tenia sentido con esta forma de hacerlo
+    @Query("SELECT m FROM Mensaje m WHERE (m.remitente = cliente AND m.destinatario = aEnviar) OR (m.remitente = aEnviar AND m.destinatario = cliente)")
+    public List<Mensaje> getMensajeList(@Param("cliente") Usuario cliente, @Param("aEnviar") Usuario aEnviar) throws SQLException;
 
-    public List<Mensaje> obtener(Usuario usuario) throws SQLException;
-
-    public boolean borrarEntre(Usuario remitente, Usuario destinatario) throws Exception;
-
+    @Query("DELETE FROM Mensaje m WHERE (m.remitente = cliente AND m.destinatario = aEnviar) OR (m.remitente = aEnviar AND m.destinatario = cliente)")
+    public boolean borrarEntreUsuarios(Usuario remitente, Usuario destinatario) throws Exception;
+    @Query("DELETE FROM Mensaje m WHERE (m.usuario = usuario")
     public boolean borrarTodos(Usuario usuario) throws SQLException;
+
 }

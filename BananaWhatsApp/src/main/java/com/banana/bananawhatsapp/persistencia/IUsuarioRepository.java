a73e0b264c70538ca6f57af89bef16973200c49c
Lluís Cobos <mangachh@gmail.com>
2024-09-06 13:14:01 +0200

    espacio

--- BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/persistencia/IUsuarioRepository.java
+++ BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/persistencia/IUsuarioRepository.java
@@ -1,12 +1,14 @@
 package com.banana.bananawhatsapp.persistencia;
 
 import com.banana.bananawhatsapp.modelos.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.sql.SQLException;
 import java.util.Set;
 
 public interface IUsuarioRepository {
     public Usuario obtener(int id) throws SQLException;
+
     public Usuario crear(Usuario usuario) throws SQLException;
 
     public Usuario actualizar(Usuario usuario) throws SQLException;
